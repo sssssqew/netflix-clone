4a8e506a0d46ade32d209726e87a954cee9b3149
sssssqew <sunrise.in.my.dream@gmail.com>
2021-11-23 03:34:00 +0900

    implemented sort and search functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Movie.css'
 
-const Movie = ({ title, genres, cover, summary }) => {
+const Movie = ({ title, genres, cover, summary, year }) => {
     return (
         <div className='movie-container'>
             <img src={cover} alt={title}></img>
-            <h3>{title}</h3>
+            <h3>{title} ({year})</h3>
             <h4>{genres.join(" ")}</h4>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { Movie, Loading } from 'components'
 
+import { Input, Button } from 'components'
 import './Home.css'
 
 const Home = () => {
     const [loading, setLoading] = useState(true)
     const [movies, setMovies] = useState([])
+    const [query, setQuery] = useState('')
+    const [isSorted, setIsSorted] = useState(-1)
+    
 
     useEffect( () => {
-        fetch('https://yts.mx/api/v2/list_movies.json?limit=12')
+        fetch('https://yts.mx/api/v2/list_movies.json?limit=20')
         .then( res => res.json())
         .then( result => {
             const {data: {movies}} = result
@@ -18,18 +22,43 @@ const Home = () => {
         })
     }, [])
 
-    const homeUI = movies.map(movie => <Movie 
+    const handleChange = (e) => {
+        const { value } = e.target
+        setQuery(value)
+    }
+
+    const sortByYear = (e) => {
+        setIsSorted(isSorted * -1)
+    }
+
+    const homeUI = movies
+                        .filter(movie => {
+                            const title = movie.title.toLowerCase()
+                            const genres = movie.genres.join(' ').toLowerCase()
+                            const q = query.toLowerCase()
+                        
+                            return title.includes(q) || genres.includes(q)
+                        })
+                        .sort( (a, b) => {
+                            return (b.year - a.year) * isSorted;
+                        })
+                        .map(movie => <Movie 
                                         key={movie.id} 
                                         title={movie.title} 
                                         genres={movie.genres} 
                                         cover={movie.medium_cover_image} 
                                         summary={movie.summary}
+                                        year={movie.year}
                                        />
                                     )
 
     return (
         <>
-            {loading? <Loading/>: <div className='Home-container'><div className='Home-movies'>{homeUI}</div></div>}
+            {loading? <Loading/>: <div className='Home-container'>
+                                    <Input name='search' type='text' placeholder='Search movies ...' value={query} onChange={handleChange}/>
+                                    <Button handleClick={sortByYear}>정렬</Button>
+                                    <div className='Home-movies'>{homeUI}</div>
+                                  </div>}
         </>
     )
 }
