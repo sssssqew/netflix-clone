322f4ac902d99b49071d11a01aad38d944d7b010
sssssqew <sunrise.in.my.dream@gmail.com>
2021-11-25 20:04:17 +0900

    implemented displaying entire movies & implemented deleting movie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
     const [movies, setMovies] = useState([])
     const [query, setQuery] = useState('')
     const [isSorted, setIsSorted] = useState(-1)
+    const [limit, setLimit] = useState(6)
     const navigate = useNavigate()
 
     const likes = JSON.parse(sessionStorage.getItem('likes')) || {}
@@ -45,6 +46,21 @@ const Home = () => {
         sessionStorage.setItem('likes', JSON.stringify(likes))
     }
 
+    const handleRemove = (id) => {
+        const moviesFiltered = movies.filter(movie => movie.id !== id)
+        setMovies(moviesFiltered)
+
+        // likes 리스트에서도 해당 영화에 대한 좋아요 정보 제거
+        const likes = JSON.parse(sessionStorage.getItem('likes')) || {}
+        delete likes[id]
+        sessionStorage.setItem('likes', JSON.stringify(likes))
+
+    }
+    const displayEntireMovies = () => {
+        console.log('display all movies !')
+        setLimit(movies.length)
+    }
+
     const homeUI = movies
                         .filter(movie => {
                             const title = movie.title.toLowerCase()
@@ -56,24 +72,28 @@ const Home = () => {
                         .sort( (a, b) => {
                             return (b.year - a.year) * isSorted;
                         })
+                        .slice(0, limit)
                         .map(movie =>
-                            <Link key={movie.id}  
-                                  to='/detail'
-                                  state={{ movie }} 
-                                  style={{ textDecoration: 'none', color: 'white'}}
-                                  onClick={() => updateLikes(movie.id)}
-                            >
-                                
-                                <Movie 
-                                        title={movie.title} 
-                                        genres={movie.genres} 
-                                        cover={movie.medium_cover_image} 
-                                        summary={movie.summary}
-                                        year={movie.year}
-                                        rating={movie.rating}
-                                        likes={likes[movie.id]}
-                                       />
-                            </Link> 
+                            <div className='movie-item' key={movie.id}  >
+                                <div className='movie-delete' onClick={(e) => handleRemove(movie.id)}>X</div>
+                                <Link 
+                                    to='/detail'
+                                    state={{ movie }} 
+                                    style={{ textDecoration: 'none', color: 'white'}}
+                                    onClick={() => updateLikes(movie.id)}
+                                >
+                                    
+                                    <Movie 
+                                            title={movie.title} 
+                                            genres={movie.genres} 
+                                            cover={movie.medium_cover_image} 
+                                            summary={movie.summary}
+                                            year={movie.year}
+                                            rating={movie.rating}
+                                            likes={likes[movie.id]}
+                                        />
+                                </Link>
+                            </div> 
                                     )
 
     const toRankPage = () => {
@@ -85,6 +105,10 @@ const Home = () => {
                                     <Menu>
                                         <Button handleClick={toRankPage}>Rank</Button>
                                     </Menu>
+                                    <div className='Home-entire'>
+                                        <Button handleClick={displayEntireMovies}>See Entire Movies</Button>
+                                    </div>
+                                    
                                     <div className='Home-contents'>
                                         <Input name='search' type='text' placeholder='Search movies ...' value={query} onChange={handleChange}/>
                                         <Button handleClick={sortByYear}>정렬</Button>
